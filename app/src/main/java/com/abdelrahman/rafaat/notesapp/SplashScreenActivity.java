3e281b788bb34708447f3f30e6f8ba3f4f7311e9
AbdElRahman Rafaat Amer <abdelrahman.raafaat@gmail.com>
2022-09-07 00:34:07 +0200

    add animation to app
    - add animation on splash screen
    - add animation when search for notes
    - add animation when no notes in data base

---
+++ app/src/main/java/com/abdelrahman/rafaat/notesapp/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package com.abdelrahman.rafaat.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.abdelrahman.rafaat.notesapp.databinding.ActivityMainBinding;
+import com.abdelrahman.rafaat.notesapp.databinding.ActivitySplashScreenBinding;
+import com.abdelrahman.rafaat.notesapp.home.view.MainActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private ActivitySplashScreenBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initUI();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            }
+        }, 2000);
+    }
+
+    private void initUI() {
+        Animation bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_screen_bottom_animation);
+        binding.titleTextView.setAnimation(bottomAnimation);
+
+
+    }
+}
--- app/src/main/java/com/abdelrahman/rafaat/notesapp/home/view/MainActivity.java
+++ app/src/main/java/com/abdelrahman/rafaat/notesapp/home/view/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.PopupMenu;
 import android.widget.SearchView;
 import android.view.animation.AnimationUtils;
@@ -44,6 +45,15 @@ public class MainActivity extends AppCompatActivity implements OnNotesClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.addNoteFloatingActionButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, AddNoteActivity.class)));
+
+        search();
+        initRecyclerView();
+        initViewModel();
+        observeViewModel();
+    }
+
+    private void search(){
         binding.noteSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -56,16 +66,7 @@ public class MainActivity extends AppCompatActivity implements OnNotesClickListe
                 return true;
             }
         });
-
-
-        binding.addNoteFloatingActionButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, AddNoteActivity.class)));
-
-
-        initRecyclerView();
-        initViewModel();
-        observeViewModel();
     }
-
     private void initRecyclerView() {
         adapter = new NotesAdapter(this);
         binding.notesRecyclerview.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
@@ -95,8 +96,15 @@ public class MainActivity extends AppCompatActivity implements OnNotesClickListe
             @Override
             public void onChanged(List<Note> notes) {
                 Log.i(TAG, "onChanged: " + notes.size());
+                if (notes.isEmpty())
+                    binding.noNotesLayout.noNotesView.setVisibility(View.VISIBLE);
+                else
+                    binding.noNotesLayout.noNotesView.setVisibility(View.GONE);
+
+                binding.noSearchLayout.noFilesView.setVisibility(View.GONE);
                 adapter.setList(notes);
                 noteList = notes;
+
             }
         });
     }
@@ -121,6 +129,12 @@ public class MainActivity extends AppCompatActivity implements OnNotesClickListe
             if (note.getTitle().toLowerCase().contains(title.toLowerCase())) {
                 filteredList.add(note);
             }
+        }
+        if (filteredList.isEmpty()) {
+            binding.noNotesLayout.noNotesView.setVisibility(View.GONE);
+            binding.noSearchLayout.noFilesView.setVisibility(View.VISIBLE);
+        } else {
+            binding.noSearchLayout.noFilesView.setVisibility(View.GONE);
         }
         adapter.setList(filteredList);
     }
